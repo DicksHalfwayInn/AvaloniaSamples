6177a72154f709c64a6f6c5133ac0c07685299b8
Schofield, stuart <546wine@gmail.com>
2023-04-26 16:52:19 -0600

    Add project files.

---
+++ AvaloniaSamples/AvaloniaSamples/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaSamples.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AvaloniaSamples/AvaloniaSamples/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaSamples.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
